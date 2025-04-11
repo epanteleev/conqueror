98d455d44136c1c8f70df9e59776054cb4b65f9e
epanteleev <msl.epanteleev@gmail.com>
2025-04-11 23:33:05 +0300

    More work

--- src/RCU.h
+++ src/RCU.h
@@ -2,12 +2,12 @@
 
 #include <memory>
 #include <thread>
+#include <functional>
 
 namespace conq {
     template<typename T>
     class RCU final {
         static_assert(sizeof(std::atomic<std::shared_ptr<T>>) == 16);
-
     public:
         explicit RCU(T&& data): m_data(std::make_shared<T>(std::move(data))) {}
 
@@ -15,15 +15,15 @@ namespace conq {
             return m_data.load(std::memory_order_acquire);
         }
 
-        template<typename Fn>
-        std::shared_ptr<T> copy_and_update(Fn &&fn) {
+        std::shared_ptr<T> copy_and_update(const std::function<void(T&)>& fn) {
             std::shared_ptr<T> old_data = m_data.load(std::memory_order_acquire);
             std::shared_ptr<T> new_data{nullptr};
             do {
                 if (old_data != nullptr) {
                     new_data = std::make_shared<T>(*old_data);
                 }
-                std::forward<Fn>(fn)(*new_data.get());
+
+                fn(*new_data.get());
             } while (!m_data.compare_exchange_weak(old_data, new_data, std::memory_order_release, std::memory_order_acquire));
             return std::move(old_data);
         }
